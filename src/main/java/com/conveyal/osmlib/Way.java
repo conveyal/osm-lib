67c41714057d21b8964950184fc3a1536c72b8a6
Brandon Martin-Anderson <badhill@gmail.com>
2015-03-04 17:38:46 -0800

    create class and test for Way

---
+++ src/main/java/com/conveyal/osmlib/Way.java
@@ -0,0 +1,16 @@
+package com.conveyal.osmlib;
+
+import java.io.Serializable;
+
+public class Way extends Tagged implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public long[] nodes;
+    
+    @Override
+    public String toString() {
+        return String.format("Way with tags %s and nodes %s", tags, nodes);
+    }
+    
+}
