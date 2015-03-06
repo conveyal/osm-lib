399063b4d7e917162c6f6f9fd5ebd8097ca7b515
Brandon Martin-Anderson <badhill@gmail.com>
2015-03-05 16:07:24 -0800

    update test

--- src/test/java/com/conveyal/osmlib/OSMTest.java
+++ src/test/java/com/conveyal/osmlib/OSMTest.java
@@ -12,9 +12,7 @@ import junit.framework.TestCase;
 public class OSMTest extends TestCase {
 	public void testOSM(){
 		OSM osm = new OSM("./src/test/resources/tmp");
-				
-		Parser parser = new Parser(osm);
-		parser.parse("./src/test/resources/bangor_maine.osm.pbf" );
+		osm.loadFromPBFFile("./src/test/resources/bangor_maine.osm.pbf");
 		
 		assertEquals( osm.nodes.size(), 35747 );
 		assertEquals( osm.ways.size(), 2976 );
