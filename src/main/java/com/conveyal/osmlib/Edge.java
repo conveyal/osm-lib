acdc20e6f0e21babe1f69ee1073ca56cc2ac7f9d
Andrew Byrd <andrew@fastmail.net>
2015-03-06 01:54:21 +0100

    import more files from OTP including package info and codecs

---
+++ src/main/java/com/conveyal/osmlib/Edge.java
@@ -0,0 +1,14 @@
+package com.conveyal.osmlib;
+
+public class Edge {
+    long way;
+    long from;
+    long to;
+    int mask;
+
+    /** Overridden to return true in edges running the opposite direction. */
+    // Arrange edges in pairs: forward and back, so you always know where the companion edge is.
+//    public boolean isBack() {
+//        return false;
+//    }
+}
