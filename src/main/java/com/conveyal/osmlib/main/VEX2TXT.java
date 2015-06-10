d4cafcd73bb811418cd3bf4f541da19082c8183f
Andrew Byrd <andrew@fastmail.net>
2015-06-10 17:54:48 +0200

    buffered output to user specified file in VEX2TXT

--- src/main/java/com/conveyal/osmlib/main/VEX2TXT.java
+++ src/main/java/com/conveyal/osmlib/main/VEX2TXT.java
@@ -1,10 +1,15 @@
 package com.conveyal.osmlib.main;
 
-import com.conveyal.osmlib.*;
+import com.conveyal.osmlib.OSMEntitySink;
+import com.conveyal.osmlib.OSMEntitySource;
+import com.conveyal.osmlib.TextOutput;
+import com.conveyal.osmlib.VexInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class VEX2TXT {
@@ -13,20 +18,24 @@ public class VEX2TXT {
 
     /** This main method will stream a VEX file to text on stdout. */
     public static void main(String[] args) {
+
         // Get input and output file names
-        if (args.length < 1) {
-            System.err.println("usage: VEX2TXT input.pbf");
+        if (args.length < 2) {
+            System.err.println("usage: VEX2TXT input.pbf out.txt");
             System.exit(0);
         }
         String inputPath = args[0];
-        // Pump the entities from the PBF input directly to the VEX output.
+        String outputPath = args[1];
+
+        // Pump the entities from the VEX input directly to the text output.
         try {
-            OSMEntitySink textSink = new TextOutput(System.out);
+            OSMEntitySink textSink = new TextOutput(new BufferedOutputStream(new FileOutputStream(outputPath)));
             OSMEntitySource vexSource = new VexInput(new FileInputStream(inputPath), textSink);
             vexSource.read();
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
+
     }
 
 }
