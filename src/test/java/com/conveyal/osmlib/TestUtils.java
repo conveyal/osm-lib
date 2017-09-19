bede65e42420c5520675d1506537588031a167a7
Evan Siroky <evan.siroky@yahoo.com>
2017-09-19 12:57:00 -0700

    refactor method that generates test database names

--- src/test/java/com/conveyal/osmlib/TestUtils.java
+++ src/test/java/com/conveyal/osmlib/TestUtils.java
@@ -1,16 +1,17 @@
 package com.conveyal.osmlib;
 
-import org.apache.commons.math3.random.MersenneTwister;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TestUtils {
 
     private static final Logger LOG = LoggerFactory.getLogger(TestUtils.class);
+    private static final AtomicInteger UNIQUE_ID = new AtomicInteger(0);
     private static String pgUrl = "jdbc:postgresql://localhost/postgres";
 
     /**
@@ -40,7 +41,7 @@ public class TestUtils {
             connection = DriverManager.getConnection(pgUrl);
         } catch (SQLException e) {
             e.printStackTrace();
-            LOG.error("Error creating new database!");
+            LOG.error("Error connecting to the database!");
             return false;
         }
 
@@ -48,7 +49,7 @@ public class TestUtils {
             connection.prepareStatement(statement).execute();
         } catch (SQLException e) {
             e.printStackTrace();
-            LOG.error("Error creating new database!");
+            LOG.error("Error excecuting sql statement!");
             return false;
         }
 
@@ -68,7 +69,7 @@ public class TestUtils {
      * @return The name of the name database, or null if creation unsucessful
      */
     public static String generateNewDB() {
-        String newDBName = randomIdString();
+        String newDBName = uniqueString();
         if (executeAndClose("CREATE DATABASE " + newDBName)) {
             return newDBName;
         } else {
@@ -87,35 +88,10 @@ public class TestUtils {
     }
 
     /**
-     * Generate a random unique prefix of n lowercase letters.
-     * We can't count on sql table or schema names being case sensitive or tolerating (leading) digits.
-     * For n=10, number of possibilities is 26^10 or 1.4E14.
-     *
-     * The approximate probability of a hash collision is k^2/2H where H is the number of possible hash values and
-     * k is the number of items hashed.
-     *
-     * SHA1 is 160 bits, MD5 is 128 bits, and UUIDs are 128 bits with only 122 actually random.
-     * To reach the uniqueness of a UUID you need math.log(2**122, 26) or about 26 letters.
-     * An MD5 can be represented as 32 hex digits so we don't save much length, but we do make it entirely alphabetical.
-     * log base 2 of 26 is about 4.7, so each character represents about 4.7 bits of randomness.
-     *
-     * The question remains of whether we need globally unique IDs or just application-unique IDs. The downside of
-     * generating IDs sequentially or with little randomness is that when multiple databases are created we'll have
-     * feeds with the same IDs as older or other databases, allowing possible accidental collisions with strange
-     * failure modes.
-     *
-     *
+     * Generate a unique string.  Mostly copied from the uniqueId method of https://github.com/javadev/underscore-java
      */
-    public static String randomIdString() {
-        MersenneTwister twister = new MersenneTwister();
-        final int length = 27;
-        char[] chars = new char[length];
-        for (int i = 0; i < length; i++) {
-            chars[i] = (char) ('a' + twister.nextInt(26));
-        }
-        // Add a visual separator, which makes these easier to distinguish at a glance
-        chars[4] = '_';
-        return new String(chars);
+    public static String uniqueString() {
+        return "test_db_" + UNIQUE_ID.incrementAndGet();
     }
 }
 
