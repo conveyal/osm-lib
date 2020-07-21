3b3d5d0a1cb234bb4ef1160f48605f76d3f4046a
Evan Siroky <evan.siroky@yahoo.com>
2020-07-21 16:00:56 -0700

    Handle exceptions in VanillaExtract better

--- src/main/java/com/conveyal/osmlib/VanillaExtract.java
+++ src/main/java/com/conveyal/osmlib/VanillaExtract.java
@@ -125,19 +125,23 @@ public class VanillaExtract {
                     response.setStatus(HttpStatus.OK_200);
                     return;
                 }
-                /* TODO filter out buildings on the server side. */
-                boolean buildings = coords.length > 4 && "buildings".equalsIgnoreCase(coords[4]);
 
                 OSMEntitySink sink = OSMEntitySink.forStream(uri, outStream);
                 TileOSMSource tileSource = new TileOSMSource(osm);
                 tileSource.setBoundingBox(minLat, minLon, maxLat, maxLon);
                 tileSource.copyTo(sink);
                 response.setStatus(HttpStatus.OK_200);
-            } catch (Exception ex) {
+            } catch (IllegalArgumentException ex) {
                 LOG.error("Could not process request with bad URI format {}.", uri);
                 response.setContentType("text/plain");
                 response.setStatus(HttpStatus.BAD_REQUEST_400);
                 outStream.write("URI format: /min_lat,min_lon,max_lat,max_lon[.pbf|.vex] (all coords in decimal degrees)\n".getBytes());
+            } catch (Exception ex) {
+                LOG.error("An internal error occurred while processing {}.", uri);
+                response.setContentType("text/plain");
+                response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR_500);
+                outStream.write("An internal error occurred.".getBytes());
+                ex.printStackTrace();
             } finally {
                 outStream.close();
             }
